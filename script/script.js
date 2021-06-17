897d0e84969b0432bf68c7fa42ca258adac330d0
EwelinaEwa <ewelina.kosciukiewicz@gmail.com>
2021-06-17 16:32:14 +0200

    Add result functionality

--- script/script.js
+++ script/script.js
@@ -4,40 +4,62 @@
         document.getElementById("rock").style.display="inline";
         userIcon = 0;
         console.log(userIcon)
-    });
+        randomElement()
+    }, { once: true });
     document.getElementById("selectPaper").addEventListener("click", function () {
         document.getElementById("paper").style.display="inline";
-        userIcon = 1
+        userIcon = 1;
         console.log(userIcon)
-    });
+        randomElement()
+    }, { once: true });
     document.getElementById("selectScissors").addEventListener("click", function () {
         document.getElementById("scissors").style.display="inline";
-        userIcon = 2
+        userIcon = 2;
         console.log(userIcon)
-    });
+        randomElement()
+    }, { once: true });
 
 
-
+    function randomElement() {
     document.getElementById("computerSelection").addEventListener("click", function () {
-        var icons = [0, 1, 2];
-        var randomIcon = icons[Math.floor(Math.random() * icons.length)];
+        let icons = [0, 1, 2];
+        let randomIcon = icons[Math.floor(Math.random() * icons.length)];
         switch(randomIcon) {
             case 0:
                 document.getElementById("rockComputer").style.display="inline";
-                computerIcon = 0;
-                console.log(computerIcon)
+                resultFinal()
                 break;
             case 1:
                 document.getElementById("paperComputer").style.display="inline";
-                computerIcon = 1;
-                console.log(computerIcon)
+                resultFinal()
                 break;
             case 2:
                 document.getElementById("scissorsComputer").style.display="inline";
-                computerIcon = 3;
-                console.log(computerIcon)
+                resultFinal()
                 break;
         }
-    })
+       console.log(randomIcon)
+        function resultFinal () {
+            if (userIcon === randomIcon) {
+                document.getElementById("result").innerHTML = `It's a draw`
+            }
+            else if (userIcon === 0 && randomIcon === 2) {
+                document.getElementById("result").innerHTML = `You win`
+            }
+            else if (userIcon === 2 && randomIcon === 0) {
+                document.getElementById("result").innerHTML = `Computer wins`
+            }
+            else if (userIcon < randomIcon) {
+                document.getElementById("result").innerHTML = `Computer wins`
+            }
+            else {
+                document.getElementById("result").innerHTML = `You win`
+            }
+        }
+
+    }, { once: true });
+
+
+};
 
 })();
